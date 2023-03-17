365b5b30a5a5d7f64052663af74351aaa639d2c3
Daksh Bhatia <72036811+dakshbhatia45@users.noreply.github.com>
2023-03-18 00:00:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+var typed = new Typed('.typer', {
+    strings: ['Developer.','Technological Consultant.','Content Writer.',],
+    typeSpeed:100,
+    backSpeed:100,
+    loop:true
+  });
+
+  let valueNumbers=document.querySelectorAll(".exp-number");
+  let interval=2000;
+
+  valueNumbers.forEach((valueNumber)=>{
+    let startValue=0;
+    let endValue=parseInt(valueNumber.getAttribute("data-val"));
+    let duration=Math.floor(interval/endValue);
+    let counter=setInterval(()=>{
+        startValue+=1;
+        valueNumber.textContent=startValue;
+        if(startValue==endValue){
+            clearInterval(counter);
+        }
+    },duration)
+  });
+
+  document.addEventListener("click",function(e){
+    if(e.target.classList.contains("gallery-item")){
+      const src=e.target.getAttribute("src");
+      document.querySelector(".modal-img").src=src;
+      const myModal=new bootstrap.Modal(document.getElementById('gallery-modal'))
+      myModal.show();
+    }
+  });
